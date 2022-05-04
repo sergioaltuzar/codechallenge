a7489116519759e537c83630b4b18068f2236946
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-05-04 15:02:04 -0500

    tronando prueba reader.test.js

--- test/utils/reader.test.js
+++ test/utils/reader.test.js
@@ -3,6 +3,6 @@ const Reader = require("./../../lib/utils/reader");
 describe("Unit test for Reader class", () => {
     test("Read json file", () => {
         const partners = Reader.readJsonFile("visualpartners.json");
-        expect(partners).not.toBe(false);
+        expect(partners).not.toBe(true);
     });
 });
