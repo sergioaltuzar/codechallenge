e05f11e30b2b43036f551e3d592f929bb0fe0885
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-05-04 15:01:37 -0500

    Create reader.test.js

---
+++ test/utils/reader.test.js
@@ -0,0 +1,8 @@
+const Reader = require("./../../lib/utils/reader");
+
+describe("Unit test for Reader class", () => {
+    test("Read json file", () => {
+        const partners = Reader.readJsonFile("visualpartners.json");
+        expect(partners).not.toBe(false);
+    });
+});
