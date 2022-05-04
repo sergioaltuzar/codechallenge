955c16fdf17ba5279e4767a1839de640f8b84251
Sergio Altuzar <77743897+sergioaltuzar@users.noreply.github.com>
2022-05-04 14:46:53 -0500

    Create studentService.test.js

---
+++ test/services/studentService.test.js
@@ -0,0 +1,29 @@
+
+   
+const StudentService = require("./../../lib/services/studentService");
+const Reader = require("./../../lib/utils/reader");
+
+describe("StudentService", () => {
+    test("Prueba para getAllStudents", () => {
+        const partners = Reader.readJsonFile("visualpartners.json");
+        const partner = StudentService.getAllStudents(partners);
+
+        expect(partner).toBeDefined();
+    });
+
+    test("Prueba para emails de estudiantes", () => {
+        const partners = Reader.readJsonFile("visualpartners.json");
+        const partner = StudentService.getEmails(partners);
+        
+        expect(partner[0]).toBe("Todd@visualpartnership.xyz");
+    });
+
+    test("Prueba estudiantes con mas de 500 creditos", () => {
+        const partners = Reader.readJsonFile("visualpartners.json");
+        const partner = StudentService.getStudentsMore500Credits(partners);
+        let status;
+        partner.forEach((element) => element.credits > 500 ? status = true : status = false);
+        
+        expect(status).toBe(true);
+    });
+});
